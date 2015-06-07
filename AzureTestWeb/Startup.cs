f55181b08387150f30830a87ff3408972546ba23
Thomas Freudenberg <info@thomasfreudenberg.com>
2015-06-07 15:10:02 +0200

    initial commit

---
+++ AzureTestWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureTestWeb.Startup))]
+namespace AzureTestWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
