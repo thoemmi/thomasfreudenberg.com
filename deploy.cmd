d416214273bf806322a7ce56cddfa0cf1b3a1199
Thomas Freudenberg <info@thomasfreudenberg.com>
2015-07-19 19:12:06 +0200

    changed deployment

--- deploy.cmd
+++ deploy.cmd
@@ -67,9 +67,13 @@ IF NOT DEFINED MSBUILD_PATH (
 
 echo Handling .NET Web Application deployment.
 
-:: 1. Restore NuGet packages
-IF /I "AzureTestWeb.sln" NEQ "" (
-  call :ExecuteCmd pretzel.exe bake --destination=%DEPLOYMENT_TARGET%
+:: 1. call pretzel
+call :ExecuteCmd pretzel.exe bake --destination=%DEPLOYMENT_TEMP%
+IF !ERRORLEVEL! NEQ 0 goto error
+
+:: 2. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
