6f2594d9392e6cf3d790535e1b905086485198c0
Thomas Freudenberg <info@thomasfreudenberg.com>
2015-11-08 17:47:13 +0100

    added 404 error page

---
+++ 404.aspx
@@ -0,0 +1,1 @@
+<%@ Page Title="" Language="C#" AutoEventWireup="true" CodeFile="404.aspx.cs" Inherits="_404" %>
---
+++ 404.aspx.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.IO;
+
+public partial class _404 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.TrySkipIisCustomErrors = true;
+        Response.Status = "404 Not Found";
+        Response.StatusCode = 404;
+        Response.WriteFile(Server.MapPath("404.html"));
+        Response.End();
+    }
+}
